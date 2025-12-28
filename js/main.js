5c165b52f13b1df5b6bc864997f74c0f90d06aaa
shpaks32 <borisenkomaksim10@gmail.com>
2025-12-28 15:19:04 +0200

    Add pr5_3

--- js/main.js
+++ js/main.js
@@ -7,7 +7,86 @@ const logInForm = document.querySelector('#logInForm')
 const loginInput = document.querySelector('#login')
 const passwordInput = document.querySelector('#password')
 
+const cardsRestaurants = document.querySelector('.cards-restaurants')
+
 let login = localStorage.getItem('gloDelivery')
+
+const dbRestaurants = [
+  {
+    image: 'img/pizza-plus/preview.jpg',
+    title: 'Піца плюс',
+    time: 50,
+    rating: 4.5,
+    price: 200,
+    category: 'Піца'
+  },
+  {
+    image: 'img/tanuki/preview.jpg',
+    title: 'Танукі',
+    time: 60,
+    rating: 4.5,
+    price: 1200,
+    category: 'Суші, роли'
+  },
+  {
+    image: 'img/food-band/preview.jpg',
+    title: 'FoodBand',
+    time: 40,
+    rating: 4.5,
+    price: 150,
+    category: 'Піца'
+  },
+  {
+    image: 'img/palki-skalki/preview.jpg',
+    title: 'Ikigai',
+    time: 55,
+    rating: 4.5,
+    price: 250,
+    category: 'Суші'
+  },
+  {
+    image: 'img/gusi-lebedi/preview.jpg',
+    title: 'Пузата хата',
+    time: 75,
+    rating: 4.5,
+    price: 300,
+    category: 'Українські страви'
+  },
+  {
+    image: 'img/pizza-burger/preview.jpg',
+    title: 'PizzaBurger',
+    time: 45,
+    rating: 4.5,
+    price: 700,
+    category: 'Піца'
+  }
+]
+
+function createCardRestaurant ({ image, title, time, rating, price, category }) {
+  const card = `
+    <a href="restaurant.html" class="card card-restaurant">
+      <img src="${image}" alt="${title}" class="card-image"/>
+      <div class="card-text">
+        <div class="card-heading">
+          <h3 class="card-title">${title}</h3>
+          <span class="card-tag tag">${time} хв</span>
+        </div>
+        <div class="card-info">
+          <div class="rating">${rating}</div>
+          <div class="price">від ${price} ₴</div>
+          <div class="category">${category}</div>
+        </div>
+      </div>
+    </a>
+  `
+
+  cardsRestaurants.insertAdjacentHTML('beforeend', card)
+}
+
+function renderCards () {
+  cardsRestaurants.textContent = ''
+  dbRestaurants.forEach(createCardRestaurant)
+}
 
 function toggleModalAuth () {
   modalAuth.classList.toggle('is-open')
@@ -98,4 +177,17 @@ function checkAuth () {
   }
 }
 
+cardsRestaurants.addEventListener('click', function (event) {
+  const target = event.target
+  const card = target.closest('.card-restaurant')
+
+  if (card) {
+    if (!login) {
+      event.preventDefault()
+      toggleModalAuth()
+    }
+  }
+})
+
+renderCards()
 checkAuth()
